8715d386e8bf620a2d2d85870453cfb0d50ede6c
SamrathNasa <88262109+SamrathNasa@users.noreply.github.com>
2022-03-12 12:47:36 +1100

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyDnNpSfp4CQlnqNDj69RFIjH8_LOgMV3nY",
+      authDomain: "kwitter-aa91c.firebaseapp.com",
+      projectId: "kwitter-aa91c",
+      storageBucket: "kwitter-aa91c.appspot.com",
+      messagingSenderId: "711805760063",
+      appId: "1:711805760063:web:a70ea4c2639cd71de6fe8a"
+    };
+
+    function logout(){
+      window.location="index.html"; 
+    }
